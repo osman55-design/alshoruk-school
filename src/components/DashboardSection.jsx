7834645684a9c066ccfbec8ddb505968a623e800
osman55-design <osmansomy55@gmail.com>
2026-08-19 21:22:53 +0300

    Refactor DashboardSection component for user management

--- src/components/DashboardSection.jsx
+++ src/components/DashboardSection.jsx
@@ -29,7 +29,7 @@ export default function DashboardSection({ users, setUsers, onBack }) {
 
     try {
       // 2. رابط الـ Web App السحابي الموحد الخاص بك المنتهي بـ /exec
-      const googleScriptUrl = "https://google.com"; 
+      const googleScriptUrl = "https://script.google.com/macros/s/AKfycbw72pRba1ZaqRXHqsnkCkoS1ebAbyzTwIaeMpP6M8AIJM6tbg4hTfuKV4E9_PG5949n/exec"; 
 
       // 3. إرسال طلب الحفظ كـ text/plain لكسر حظر الـ CORS تماماً في المتصفحات
       const response = await fetch(googleScriptUrl, {
